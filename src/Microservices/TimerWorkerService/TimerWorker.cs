4fdd9a6aaaccb1ae1e531e169267ef2c45233849
Shimon dahan <Shimond@any-techs.co.il>
2022-06-16 14:31:34 +0300

    Event Bus fix

--- src/Microservices/TimerWorkerService/TimerWorker.cs
+++ src/Microservices/TimerWorkerService/TimerWorker.cs
@@ -7,19 +7,23 @@ public class TimerWorker : BackgroundService
 {
     private readonly ILogger<TimerWorker> _logger;
     private readonly IEventBus _eventBus;
-
+    private DateTime _lastTime;
     public TimerWorker(ILogger<TimerWorker> logger, IEventBus eventBus)
     {
         _logger = logger;
         _eventBus = eventBus;
+        _lastTime = DateTime.Today;
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         while (!stoppingToken.IsCancellationRequested)
         {
-            await Task.Delay(1000, stoppingToken);
-            await _eventBus.Publish(new DatePassedEvent());
+            await Task.Delay(10000, stoppingToken);
+            if (DateTime.Now.Subtract(_lastTime).Days == 1)
+            {
+                await _eventBus.Publish(new DatePassedEvent());
+            }
         }
     }
 }
